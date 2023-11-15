4528392a3aae18b91da8c2a419ffd01ef7f7f224
unknown <himaja.vj.hs@gmail.com>
2023-11-15 20:36:23 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './features/components/category-list/category-list.component';
+import { AddCategoryComponent } from './features/components/add-category/add-category.component';
+import { EditCategoryComponent } from './features/components/edit-category/edit-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'admin/categories',
+    component : CategoryListComponent
+  },
+  {
+    path : 'admin/categories/add',
+    component : AddCategoryComponent
+  },
+  {
+    path : 'admin/categories/:id',
+    component : EditCategoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Core/Components/navbar/navbar.component';
+import { CategoryListComponent } from './features/components/category-list/category-list.component';
+import { AddCategoryComponent } from './features/components/add-category/add-category.component';
+import { FormsModule } from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { EditCategoryComponent } from './features/components/edit-category/edit-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryListComponent,
+    AddCategoryComponent,
+    EditCategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
